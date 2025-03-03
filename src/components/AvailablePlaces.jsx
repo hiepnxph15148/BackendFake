781993ac2327e4334bdf6e36d49b78e11e8a8f07
hiepnxph <hiepnxph@gmail.com>
2025-03-04 00:44:23 +0700

    Update

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,24 +1,43 @@
 import { useState } from "react";
 import Places from "./Places.jsx";
 import { useEffect } from "react";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   // Todo : fetch available places from backend API
+  const [isFetching, setIsFetching] = useState(false);
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchPlases() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+    async function fetchPlaces() {
+      setIsFetching(true);
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json(); 
+        if (!response.ok) {
+          throw new Error("Failed to fetch places.");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || "error"
+        });
+      }
+        setIsFetching(false);
     }
-    fetchPlases();
+    fetchPlaces();
   }, []);
-
+  if(error) {
+    return <Error title="An error occurred!" message={error.message} />;
+  }
+  // Render available places
   return (
     <Places
       title="Available Places"
       places={availablePlaces}
+      loadingText="Loading available places..."
+      isLoading={isFetching}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
